a5b38b8c0608337f2ea21e6e0f854637078cdca2
John Mileham <john@betterment.com>
2015-09-30 19:14:07 -0400

    Be paranoid about the block actually being run

--- spec/test_track_rails_spec.rb
+++ spec/test_track_rails_spec.rb
@@ -4,9 +4,12 @@ module TestTrackRails
   RSpec.describe TestTrackRails do
     describe ".update_config" do
       it "yields an updater" do
+        block_execution_count = 0
         TestTrackRails.update_config do |c|
           expect(c).to be_a(ConfigUpdater)
+          block_execution_count += 1
         end
+        expect(block_execution_count).to eq 1
       end
     end
   end
