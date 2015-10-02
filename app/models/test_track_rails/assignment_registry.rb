3de04618aabd3217888997bbbcaa827e18c368f9
John Mileham <john@betterment.com>
2015-10-02 00:32:26 -0400

    Bake in assignments

--- app/models/test_track_rails/assignment_registry.rb
+++ app/models/test_track_rails/assignment_registry.rb
@@ -1,17 +1,16 @@
 module TestTrackRails
-  class SplitRegistry
+  class AssignmentRegistry
     include TestTrackModel
 
-    collection_path '/api/split_registry'
-
     def self.fake_instance_attributes(_)
-      { time: { hammertime: 100 } }
+      { time: 'hammertime' }
     end
 
-    def self.instance
+    def self.for_visitor(visitor_id)
+      raise "must provide a visitor_id" unless visitor_id
       # TODO: FakeableHer needs to make this faking a feature of `get`
       if ENV['TEST_TRACK_ENABLED']
-        get('/api/split_registry')
+        get("/api/visitors/#{URI.escape(visitor_id)}/assignment_registry")
       else
         new(fake_instance_attributes(nil))
       end
--- spec/helpers/test_track_rails/application_helper_spec.rb
+++ spec/helpers/test_track_rails/application_helper_spec.rb
@@ -2,22 +2,29 @@ require 'rails_helper'
 
 RSpec.describe TestTrackRails::ApplicationHelper do
   describe "#test_track_setup_tag" do
-    let(:url) { "http://dummy:fakepassword@testtrack.dev/api/split_registry" }
+    let(:split_registry_url) { "http://dummy:fakepassword@testtrack.dev/api/split_registry" }
+    let(:weightings) { { time: { hammertime: 0, millertime: 100 } } }
 
-    let(:weightings) { { hammertime: 0, millertime: 100 } }
+    let(:visitor_id) { SecureRandom.uuid }
+    let(:assignment_registry_url) { "http://dummy:fakepassword@testtrack.dev/api/visitors/#{visitor_id}/assignment_registry" }
+    let(:assignments) { { time: "hammertime" } }
 
     before do
-      stub_request(:get, url).to_return(body: weightings.to_json)
+      stub_request(:get, split_registry_url).to_return(body: weightings.to_json)
+      stub_request(:get, assignment_registry_url).to_return(body: assignments.to_json)
     end
 
     it "returns a reasonable-looking script tag" do
-      result = with_env(TEST_TRACK_ENABLED: 1) { result = helper.test_track_setup_tag }
+      cookies[:tt_visitor_id] = visitor_id
+
+      result = with_env(TEST_TRACK_ENABLED: 1) { helper.test_track_setup_tag }
 
       expect(result).to include("<script>")
       expect(result).to include("window.TEST_TRACK = {")
       expect(result).to include("host: 'http://dummy:fakepassword@testtrack.dev'")
       expect(result).to include("cookieDomain: '*.test.host',")
-      expect(result).to include('registry: {"hammertime":0,"millertime":100}')
+      expect(result).to include('registry: {"time":{"hammertime":0,"millertime":100}}')
+      expect(result).to include('assignments: {"time":"hammertime"}')
     end
   end
 end
