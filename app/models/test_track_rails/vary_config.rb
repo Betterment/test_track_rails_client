5c6d32444a6fc085bbd5c3995910e59c7f61b3f0
Alan Norton <me@alannorton.com>
2015-10-27 11:07:05 -0400

    stub updating of assigment to visitor

--- app/models/test_track_rails/vary_config.rb
+++ app/models/test_track_rails/vary_config.rb
@@ -1,38 +1,54 @@
 module TestTrackRails
   class VaryConfig
-    def initialize(split_name, assignment, split_registry)
+    attr_reader :options, :branches, :assigned_variant_name, :default_variant_name
+
+    def initialize(split_name, assigned_variant_name, split_registry)
+      @assigned_variant_name = assigned_variant_name.to_s
       @options = split_registry[split_name.to_s].keys
-      @assignment = assignment.to_s
       @branches = {}
     end
 
-    def when(assignment_name, &_block)
-      raise ArgumentError, "must provide block to `when` for #{assignment_name}" unless block_given?
-      _errbit(assignment_name) unless @options.include? assignment_name.to_s
+    def when(variant_name, &_block)
+      raise ArgumentError, "must provide block to `when` for #{variant_name}" unless block_given?
+      _errbit(variant_name) unless options.include? variant_name.to_s
 
-      @branches[assignment_name.to_s] = proc
+      branches[variant_name.to_s] = proc
     end
 
-    def default(assignment_name, &_block)
-      raise ArgumentError, "must provide block to `default` for #{assignment_name}" unless block_given?
-      _errbit(assignment_name) unless @options.include? assignment_name.to_s
+    def default(default_variant_name, &_block) # rubocop:disable Metrics/AbcSize
+      raise ArgumentError, "must provide block to `default` for #{variant_name}" unless block_given?
       raise ArgumentError, "cannot provide more than one `default`" unless @default.nil?
+      _errbit(variant_name) unless @options.include? variant_name.to_s
 
-      @default = assignment_name
-      @branches[assignment_name.to_s] = proc
+      @default_variant_name = default_variant_name
+      branches[variant_name.to_s] = proc
     end
 
     private
 
-    def _errbit(assignment_name)
-      puts "#{@options} must include #{assignment_name}" # rubocop:disable Rails/Output
+    def default_branch
+      branches[default_variant_name]
+    end
+
+    def _errbit(variant_name)
+      puts "#{options} must include #{variant_name}" # rubocop:disable Rails/Output
+    end
+
+    def _assign_visitor_to_default_variant
+      puts "not sure how to do that" # rubocop:disable Rails/Output
     end
 
     def run
       raise ArgumentError, "must provide exactly one `default`" unless @default
       raise ArgumentError, "must provide at least one `when`" unless @branches.size >= 2
 
-      chosen_path = @branches[@assignment].nil? ? @branches[@default] : @branches[@assignment]
+      if branches[assigned_variant_name].present?
+        chosen_path = branches[assigned_variant_name]
+      else
+        chosen_path = default_branch
+        _assign_visitor_to_default_variant
+      end
+
       chosen_path.call
     end
   end
