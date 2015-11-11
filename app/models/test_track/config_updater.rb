9e4ee49d76d72f698618dc23a769a4d594dd49f7
Adam Langsner <adamlangsner@gmail.com>
2015-11-11 08:55:34 -0500

    persisting test track migrations to schema file

--- app/models/test_track/config_updater.rb
+++ app/models/test_track/config_updater.rb
@@ -1,9 +1,40 @@
 class TestTrack::ConfigUpdater
   def split(name, weighting_registry)
     TestTrack::SplitConfig.create!(name: name, weighting_registry: weighting_registry)
+    splits[name.to_s] = weighting_registry.stringify_keys
+    persist_schema!
   end
 
   def identifier_type(name)
     TestTrack::IdentifierType.create!(name: name)
+    identifier_types << name.to_s
+    persist_schema!
+  end
+
+  private
+
+  def persist_schema!
+    file = File.open(schema_file_name, "w")
+    YAML.dump({ "identifier_types" => identifier_types.to_a, "splits" => splits }, file)
+  end
+
+  def identifier_types
+    @identifier_types ||= Set.new(schema_file_hash["identifier_types"] || [])
+  end
+
+  def splits
+    @splits ||= schema_file_hash["splits"] || {}
+  end
+
+  def schema_file_hash
+    @schema_hash ||= YAML.load(schema_file_contents) || {}
+  end
+
+  def schema_file_contents
+    @schema_file_contents ||= File.open(schema_file_name, "a+").read
+  end
+
+  def schema_file_name
+    @schema_file_name ||= "#{Rails.root}/db/test_track_schema.yml"
   end
 end
--- spec/models/test_track/config_updater_spec.rb
+++ spec/models/test_track/config_updater_spec.rb
@@ -1,11 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe TestTrack::ConfigUpdater do
+  let(:filename) { "#{Rails.root}/db/test_track_schema.yml" }
+  let(:file) { instance_double(File, read: "") }
+
+  before do
+    allow(File).to receive(:open).and_return(file)
+    allow(YAML).to receive(:dump).and_return(file)
+  end
+
   describe "#split" do
     it "updates split_config" do
       allow(TestTrack::SplitConfig).to receive(:create!).and_call_original
       expect(subject.split(:name, foo: 20, bar: 80)).to be_truthy
       expect(TestTrack::SplitConfig).to have_received(:create!).with(name: :name, weighting_registry: { foo: 20, bar: 80 })
+    end
+
+    it "persists the splits to a schema file" do
+      subject.split(:name, foo: 20, bar: 80)
+
+      expect(File).to have_received(:open).with(filename, "a+")
+      expect(File).to have_received(:open).with(filename, "w")
+      expect(YAML).to have_received(:dump).with({ "identifier_types" => [], "splits" => { "name" => { "foo" => 20, "bar" => 80 } } }, file)
+    end
+
+    it "does not overwrite what is currently in the schema file" do
+      allow(YAML).to receive(:load).and_return(
+        "identifier_types" => ["some_identifier_type"],
+        "splits" => {
+          "blue_button" => { "true" => 50, "false" => 50 }
+        }
+      )
+
+      subject.split(:name, foo: 20, bar: 80)
+
+      expect(YAML).to have_received(:dump).with(
+        {
+          "identifier_types" => ["some_identifier_type"],
+          "splits" => {
+            "blue_button" => { "true" => 50, "false" => 50 },
+            "name" => { "foo" => 20, "bar" => 80 }
+          }
+        },
+        file
+      )
     end
   end
 
@@ -14,6 +52,35 @@ RSpec.describe TestTrack::ConfigUpdater do
       allow(TestTrack::IdentifierType).to receive(:create!).and_call_original
       expect(subject.identifier_type(:my_id)).to be_truthy
       expect(TestTrack::IdentifierType).to have_received(:create!).with(name: :my_id)
+    end
+
+    it "persists the identifier types to a schema file" do
+      subject.identifier_type(:my_id)
+
+      expect(File).to have_received(:open).with(filename, "a+")
+      expect(File).to have_received(:open).with(filename, "w")
+      expect(YAML).to have_received(:dump).with({ "identifier_types" => ["my_id"], "splits" => {} }, file)
+    end
+
+    it "does not overwrite what is currently in the schema file" do
+      allow(YAML).to receive(:load).and_return(
+        "identifier_types" => ["some_identifier_type"],
+        "splits" => {
+          "blue_button" => { "true" => 50, "false" => 50 }
+        }
+      )
+
+      subject.identifier_type(:my_id)
+
+      expect(YAML).to have_received(:dump).with(
+        {
+          "identifier_types" => %w(some_identifier_type my_id),
+          "splits" => {
+            "blue_button" => { "true" => 50, "false" => 50 }
+          }
+        },
+        file
+      )
     end
   end
 end
