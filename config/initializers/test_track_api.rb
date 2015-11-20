dfbec650a00eded4ae1608864413f6c7650fbb11
Adam Langsner <adamlangsner@gmail.com>
2015-11-19 21:01:22 -0500

    allow timeout override

--- config/initializers/test_track_api.rb
+++ config/initializers/test_track_api.rb
@@ -10,6 +10,6 @@ TestTrack::TestTrackApi = Her::API.new.setup url: ENV['TEST_TRACK_API_URL'] do |
   c.adapter Faraday.default_adapter
 
   # Set aggressive HTTP timeouts because TestTrack needs to be fast
-  c.options[:open_timeout] = 2 # Number of seconds to wait for the connection to open.
-  c.options[:timeout] = 4 # Number of seconds to wait for one block to be read (via one read(2) call).
+  c.options[:open_timeout] = ENV['TEST_TRACK_OPEN_TIMEOUT'] || 2 # Number of seconds to wait for the connection to open.
+  c.options[:timeout] = ENV['TEST_TRACK_TIMEOUT'] || 4 # Number of seconds to wait for one block to be read (via one read(2) call).
 end
