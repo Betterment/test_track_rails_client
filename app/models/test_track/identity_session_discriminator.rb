09f0c752b4d48b5e503d99e4e5a26f84fc8942f1
Adam Langsner <adamlangsner@gmail.com>
2016-01-08 15:28:09 -0500

    refactor IdentitySessionDiscriminator predicate methods

--- app/models/test_track/identity_session_discriminator.rb
+++ app/models/test_track/identity_session_discriminator.rb
@@ -10,17 +10,17 @@ class TestTrack::IdentitySessionDiscriminator
   end
 
   def participate_in_online_session?
-    web_context? && controller_has_authenticated_resource? && authenticated_resource_matches_identity?
+    authenticated_resource_matches_identity?
   end
 
   private
 
   def authenticated_resource_matches_identity?
-    controller.send(authenticated_resource_method_name) == identity
+    controller_has_authenticated_resource? && controller.send(authenticated_resource_method_name) == identity
   end
 
   def controller_has_authenticated_resource?
-    controller.respond_to?(authenticated_resource_method_name)
+    web_context? && controller.respond_to?(authenticated_resource_method_name)
   end
 
   def web_context?
