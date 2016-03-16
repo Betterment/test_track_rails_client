a13a10d38bd4e9d6ab5cb718f6a62546b6bf4b7f
Christopher Zega <zega@betterment.com>
2016-03-16 18:43:13 -0400

    remove unneeded code

--- app/helpers/test_track/application_helper.rb
+++ app/helpers/test_track/application_helper.rb
@@ -1,7 +1,6 @@
 module TestTrack::ApplicationHelper
   def test_track_setup_tag
-    state_json = test_track_session.state_hash.to_json
-    state_json_base64 = Base64.strict_encode64(state_json)
+    state_json_base64 = Base64.strict_encode64(test_track_session.state_hash.to_json)
     js = javascript_tag("window.TT = '#{state_json_base64}';")
     content_tag(:div, js, class: '_tt', style: 'visibility:hidden;width:0;height:0;')
   end
