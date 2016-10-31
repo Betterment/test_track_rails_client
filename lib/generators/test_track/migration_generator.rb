71d2a23098d2a6a52f78c58248d894528c7fced9
John Turner <johnathan.turner@betterment.com>
2016-10-31 11:55:52 -0400

    Add gate and experiment flags

--- lib/generators/test_track/migration_generator.rb
+++ lib/generators/test_track/migration_generator.rb
@@ -1,16 +1,22 @@
-require 'rails/generators/named_base'
+require 'rails/generators/base'
 
 module TestTrack
   module Generators
-    class MigrationGenerator < Rails::Generators::NamedBase
+    class MigrationGenerator < Rails::Generators::Base
       desc "Creates a test track migration file. Files that start with retire or finish will create migrations that finish a split."
 
+      class_option :experiment, aliases: 'e', type: :boolean, desc: 'Set up the split as an experiment'
+      class_option :gate, aliases: 'g', type: :boolean, desc: 'Set up the split as a gate/feature flag'
+
+      argument :file_name, required: true
+
       def create_test_track_migration_file
+        validate_options!
         create_file "db/migrate/#{formatted_time_stamp}_#{file_name}.rb", <<-FILE.strip_heredoc
           class #{file_name.camelize} < ActiveRecord::Migration
             def change
               TestTrack.update_config do |c|
-                #{split_command} :#{split_name}
+                #{split_command_line}
               end
             end
           end
@@ -19,8 +25,12 @@ module TestTrack
 
       private
 
+      def split_command_line
+        "#{split_command} :#{split_name}, #{split_variants}"
+      end
+
       def formatted_time_stamp
-        Time.zone.now.strftime('%Y%m%d%H%M%S')
+        Time.now.strftime('%Y%m%d%H%M%S')
       end
 
       def split_command
@@ -32,7 +42,35 @@ module TestTrack
       end
 
       def split_name
-        file_name.split('_').slice(1, file_name.length).join('_')
+        name = file_name.split('_').slice(1, file_name.length).join('_')
+        if gate?
+          name += '_enabled'
+        elsif experiment?
+          name += '_experiment'
+        end
+        name
+      end
+
+      def split_variants
+        if gate?
+          'true: 0, false: 100'
+        elsif experiment?
+          'control: 50, treatment: 50'
+        else
+          'control: 100, treatment: 0'
+        end
+      end
+
+      def validate_options!
+        raise 'Cannot use both --gate and --experiment. Please choose one' if gate? && experiment?
+      end
+
+      def gate?
+        options[:gate].present?
+      end
+
+      def experiment?
+        options[:experiment].present?
       end
     end
   end
