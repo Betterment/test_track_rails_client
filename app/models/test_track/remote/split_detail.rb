4f17d026d75f7e0915a7c63bcdf03b242efb3004
agirlnamedsophia <sophia.russell@betterment.com>
2017-04-05 15:19:27 -0400

    name!

--- app/models/test_track/remote/split_detail.rb
+++ app/models/test_track/remote/split_detail.rb
@@ -5,14 +5,14 @@ class TestTrack::Remote::SplitDetail
 
   attributes :name
 
-  def self.from_name(identifier_value)
-    raise "must provide an identifier_value" unless identifier_value.present?
+  def self.from_name(name)
+    raise "must provide a name" unless name.present?
 
     # TODO: FakeableHer needs to make this faking a feature of `get`
     if faked?
       new(fake_instance_attributes(nil))
     else
-      get("/api/v1/split_details/#{identifier_value}")
+      get("/api/v1/split_details/#{name}")
     end
   end
 
