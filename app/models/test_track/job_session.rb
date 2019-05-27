4f3be47c0c4352ab51f3791a9980c0ff16a887bf
Alex Burgel <aburgel@betterment.com>
2019-05-27 12:58:03 -0400

    Manage request store within JobSession

--- app/models/test_track/job_session.rb
+++ app/models/test_track/job_session.rb
@@ -1,13 +1,18 @@
 class TestTrack::JobSession
   def manage
     raise ArgumentError, "must provide block to `manage`" unless block_given?
+    raise "already in use" unless RequestStore[:test_track_job_session].nil?
 
+    RequestStore[:test_track_job_session] = self
     yield
   ensure
     notify_unsynced_assignments!
+    RequestStore[:test_track_job_session] = nil
   end
 
   def visitor_dsl_for(identity)
+    raise "must be called within `manage` block" if RequestStore[:test_track_job_session].nil?
+
     TestTrack::VisitorDSL.new(for_identity(identity))
   end
 
--- lib/delayed/plugins/job_session_plugin.rb
+++ lib/delayed/plugins/job_session_plugin.rb
@@ -4,7 +4,6 @@ module Delayed
         callbacks do |lifecycle|
           lifecycle.around(:invoke_job) do |job, *args, &block|
             test_track_job_session = TestTrack::JobSession.new
-            RequestStore[:test_track_job_session] = test_track_job_session
 
             test_track_job_session.manage do
               block.call(job, *args)
