7fe9f462b2d7b78269ba0e39f285ed792a03acb0
smudge <nathan@ngriffith.com>
2020-04-17 09:49:43 -0400

    No need to load engine tasks here anymore

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,11 +16,6 @@ require 'timecop'
 # load all files in support folders
 Dir[TestTrackRailsClient::Engine.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
 
-# load rake tasks
-require 'rake'
-APP_RAKEFILE = File.expand_path('dummy/Rakefile', __dir__)
-load 'rails/tasks/engine.rake'
-
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
