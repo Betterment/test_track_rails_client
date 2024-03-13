712bfc4ba116ce1f4e35dfe6930f2eb98b8a6cd5
Nathan Griffith <nathan@betterment.com>
2024-03-12 22:22:20 -0400

    Set build timestamp initializer order (#126)
    I noticed that some recent changes to my load order in a consuming app resulted in this initializer running too soon, before `Time.zone` was defined by Rails. This change forces it to always run _after_ `'active_support.initialize_time_zone'`.

--- lib/test_track_rails_client/engine.rb
+++ lib/test_track_rails_client/engine.rb
@@ -9,5 +9,9 @@ module TestTrackRailsClient
     config.generators do |g|
       g.test_framework :rspec
     end
+
+    initializer after: 'active_support.initialize_time_zone' do
+      TestTrack.set_build_timestamp! unless ENV['SKIP_TESTTRACK_SET_BUILD_TIMESTAMP'] == '1'
+    end
   end
 end
