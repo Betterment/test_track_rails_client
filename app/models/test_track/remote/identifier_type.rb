478505b98abd0532d5786af1e5a36718661de4c2
Ray Zane <raymondzane@gmail.com>
2025-01-22 16:26:06 -0500

    Implement IdentifierType

--- app/models/test_track/remote/identifier_type.rb
+++ app/models/test_track/remote/identifier_type.rb
@@ -1,11 +1,17 @@
 class TestTrack::Remote::IdentifierType
-  include TestTrack::RemoteModel
+  include TestTrack::Resource
 
-  collection_path 'api/v1/identifier_type'
-
-  attributes :name
+  attribute :name
 
   validates :name, presence: true
+
+  def save
+    return false unless valid?
+
+    body = { name: }
+    connection.post("api/v1/identifier_type", body) unless faked?
+    true
+  end
 
   def fake_save_response_attributes
     nil # :no_content is the expected response type
