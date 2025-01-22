b2b4d4e113e07ecdcb44cab4c56f9633a866ad53
Ray Zane <raymondzane@gmail.com>
2025-01-22 18:11:27 -0500

    Convert Remote::Assignment

--- app/models/test_track/remote/assignment.rb
+++ app/models/test_track/remote/assignment.rb
@@ -1,7 +1,11 @@
 class TestTrack::Remote::Assignment
-  include TestTrack::RemoteModel
+  include TestTrack::Resource
+  include ActiveModel::Dirty
 
-  attributes :split_name, :variant, :context, :unsynced
+  attribute :split_name
+  attribute :variant
+  attribute :context
+  attribute :unsynced, :boolean
 
   validates :split_name, :variant, :mixpanel_result, presence: true
 
