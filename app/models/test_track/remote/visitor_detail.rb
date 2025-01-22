f91a3680dbf98d1377d834f2add8b7bfce7ab0d7
Ray Zane <raymondzane@gmail.com>
2025-01-22 18:01:16 -0500

    Replace VisitorDetail

--- app/models/test_track/remote/visitor_detail.rb
+++ app/models/test_track/remote/visitor_detail.rb
@@ -1,15 +1,13 @@
 class TestTrack::Remote::VisitorDetail
-  include TestTrack::RemoteModel
+  include TestTrack::Resource
 
-  has_many :assignment_details
+  attr_reader :assignment_details
 
   def self.from_identifier(identifier_type, identifier_value)
-    # TODO: FakeableHer needs to make this faking a feature of `get`
-    if faked?
-      new(fake_instance_attributes(nil))
-    else
-      get("api/v1/identifier_types/#{identifier_type}/identifiers/#{identifier_value}/visitor_detail")
-    end
+    return new(fake_instance_attributes(nil)) if faked?
+
+    response = connection.get("api/v1/identifier_types/#{identifier_type}/identifiers/#{identifier_value}/visitor_detail")
+    new(response.body)
   end
 
   def self.fake_instance_attributes(_)
@@ -19,5 +17,11 @@ class TestTrack::Remote::VisitorDetail
         TestTrack::Remote::AssignmentDetail.fake_instance_attributes(nil)
       ]
     }
+  end
+
+  def assignment_details=(values)
+    @assignment_details = values.map do |value|
+      TestTrack::Remote::AssignmentDetail.new(value)
+    end
   end
 end
